4217ae4e0c1c0c4c32181040b50fcb04d16a9847
Viyan.UX Engineer <74237057+starblazingunicorn@users.noreply.github.com>
2023-07-06 20:44:09 +0200

    updated vercel

--- extra-escape/astro.config.mjs
+++ extra-escape/astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
-
-import vercel from "@astrojs/vercel/serverless";
+import vercelStatic from '@astrojs/vercel/static';
+ 
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: vercel()
+  output: 'static',
+  adapter: vercelStatic(),
 });
